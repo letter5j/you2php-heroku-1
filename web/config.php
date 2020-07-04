2b21f122871bf84620c84a49bc4bc59f8479cc20
Log <letter5j@users.noreply.github.com>
2020-07-04 16:16:47 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyAlZ_UGSaFWMIxmBedCL-O6elwSPI4R_HM');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'Hello World!');
 define('TITLENAME', 'Hello World!');
