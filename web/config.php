2a595bd3d13a9ca3fd3e085fd523e58a80490bb6
Log <letter5j@users.noreply.github.com>
2020-07-05 12:20:46 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyAvpxFAr5G14EMaOncTNOlPJLNF1d8iNbM');
@@ -7,4 +8,16 @@ define('SITE_NAME', 'betuYou');
 define('TITLENAME', 'betuYou');
 define('EN2DEKEY', '0090909i604wkkfkak');
 define('EMAIL', 'mjy20070413@gmail.com');
+*/
+?>
+
+<?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'TW';
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyDq7KxZ9l1qW1I3QPxI-gtnSDxOysMbr9s');
+define('GJ_CODE', $gl);
+define('SITE_NAME', '海量高清视频 在线观看 视频发布 视频搜索 视频分享');
+define('TITLENAME', 'YTBHK');
+define('EN2DEKEY', 'du$5^anvb|OVE&y');
+define('EMAIL', 'admin@ytbhk.com');
 ?>
